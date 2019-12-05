68486735e20064b797d5b6eb57f7dc082489ae8a
abhijitjadhav1998 <abhijit.jadhav1998@gmail.com>
2019-12-05 18:52:57 +0530

    added Admin user functionality

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_12_02_140410) do
+ActiveRecord::Schema.define(version: 2019_12_05_130131) do
 
   create_table "articles", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "title"
@@ -26,6 +26,7 @@ ActiveRecord::Schema.define(version: 2019_12_02_140410) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "password_digest"
+    t.boolean "admin", default: false
   end
 
 end
